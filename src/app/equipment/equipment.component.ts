96bdf5f39abf672fcb8f0d11353d3fea52e4f51f
rajandk25 <rajandk25@users.noreply.github.com>
2020-11-04 16:03:42 -0600

    studio-2 done

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+  equipments = ['Habitait dome', 'Drones', 'Food containeres', 'Oxygen tanks']
   constructor() { }
 
   ngOnInit() {
